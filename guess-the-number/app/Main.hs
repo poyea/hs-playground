68647b71e1ddfd78b094805bc941d509e230fc31
John Law <johnlaw.po@gmail.com>
2020-09-21 03:22:47 +0800

    Init: Add guess-a-number program

---
+++ guess-the-number/app/Main.hs
@@ -0,0 +1,36 @@
+module Main where
+
+import Control.Monad (when)
+import Lib
+import System.Exit (exitSuccess)
+
+main = do
+  askForNumber
+
+askForNumber :: IO ()
+askForNumber = do
+  putStrLn "Input a target..."
+  target <- getLine
+  if null target
+    then do
+      putStrLn "Invalid integer"
+      askForNumber
+    else do
+      let targetNumber = (read target :: Int)
+      goGuess targetNumber True
+
+goGuess :: Int -> Bool -> IO ()
+goGuess target signal = do
+  if not signal
+    then exitSuccess
+    else do
+      putStrLn "Input a guess... "
+      number <- getLine
+      if null number
+        then do
+          putStrLn "Invalid integer"
+          goGuess target True
+        else do
+          let theNumber = (read number :: Int)
+          putStrLn $ checkNumber theNumber target
+          goGuess target (theNumber /= target)
